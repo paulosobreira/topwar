ca5af80b815d64beacd29b510aefd8a9b6e27db4
sowbreira <sowbreira@gmail.com>
2015-07-24 13:08:37 -0300

    Reovendo toBufferedImage

--- topwar/src/br/nnpe/ImageUtil.java
+++ topwar/src/br/nnpe/ImageUtil.java
@@ -62,67 +62,6 @@ public class ImageUtil {
 		return bufferedImageRetorno;
 	}
 
-	// This method returns a buffered image with the contents of an image
-	public static BufferedImage toBufferedImage(Image image) {
-		if (image instanceof BufferedImage) {
-			return (BufferedImage) image;
-		}
-
-		// This code ensures that all the pixels in the image are loaded
-		image = new ImageIcon(image).getImage();
-
-		// Determine if the image has transparent pixels; for this method's
-		boolean hasAlpha = false; // hasAlpha(image);
-
-		// Create a buffered image with a format that's compatible with the
-		// screen
-		BufferedImage bimage = null;
-
-		GraphicsEnvironment ge = GraphicsEnvironment
-				.getLocalGraphicsEnvironment();
-
-		try {
-			// Determine the type of transparency of the new buffered image
-			int transparency = Transparency.OPAQUE;
-
-			if (hasAlpha) {
-				transparency = Transparency.BITMASK;
-			}
-
-			// Create the buffered image
-			GraphicsDevice gs = ge.getDefaultScreenDevice();
-
-			GraphicsConfiguration gc = gs.getDefaultConfiguration();
-
-			bimage = gc.createCompatibleImage(image.getWidth(null),
-					image.getHeight(null), transparency);
-		} catch (HeadlessException e) {
-			Logger.logar(e);
-		}
-
-		if (bimage == null) {
-			// Create a buffered image using the default color model
-			int type = BufferedImage.TYPE_INT_RGB;
-
-			if (hasAlpha) {
-				type = BufferedImage.TYPE_INT_ARGB;
-			}
-
-			bimage = new BufferedImage(image.getWidth(null),
-					image.getHeight(null), type);
-		}
-
-		// Copy image to buffered image
-		Graphics g = bimage.createGraphics();
-
-		// Paint the image onto the buffered image
-		g.drawImage(image, 0, 0, null);
-
-		g.dispose();
-
-		return bimage;
-	}
-
 	/**
 	 * Serve pra nada essa porra!!!
 	 * 
